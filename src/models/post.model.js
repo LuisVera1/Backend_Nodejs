cab6a79e3ac5add0913e514cd9c522fdeb64e322
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-06-08 22:48:12 -0500

    modify model

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -2,20 +2,9 @@ const mongoose = require("mongoose");
 
 const articleSchema = new mongoose.Schema({
   autorName: String,
-  comentarios: [
-    {
-      un_comentario: String,
-      otro_comentario: String,
-      uno_mas_comentario: String,
-    }],
+  comentarios: Array,
   date: Number,
-  fullPost: [
-    { 
-    hastag1: String,
-    hastag2: String,
-    hastag3: String,
-    hastag4: String,
-    }],
+  fullPost: Array,
   imgPost: String,
   reactions: Number,
   title: String,
